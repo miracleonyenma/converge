824a8921186009da80a5792fe356b84daf32cb1d
miracleonyenma <miracleficient@gmail.com>
2019-10-30 13:22:14 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const router = require('./router');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket)=>{
+    console.log('we have a new connection');
+
+    socket.on('disconnect', ()=>{
+        console.log("user gone")
+    })
+})
+
+app.use(router);
+server.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
