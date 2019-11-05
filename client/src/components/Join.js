1b750b3ecb9d3241ab30318eec61879ac6eeb337
miracleonyenma <miracleficient@gmail.com>
2019-11-05 11:28:15 +0100

    re init react

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,9 +1,1 @@
-import React from 'react';
-
-const Join = ()=>{
-    return(
-        <h1>Join</h1>
-    )
-}
-
-export default Join;
+import react from 'react';
--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-
-
-
-
-
-
-
-
-
+ReactDom.render(<App />, document.querySelector('#root'));
